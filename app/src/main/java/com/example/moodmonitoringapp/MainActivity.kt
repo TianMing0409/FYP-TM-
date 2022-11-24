d60b6aa345f8ac3e602b5f30b4e23e8d42b248cb
TianMing0409 <tianmingkoh@gmail.com>
2022-11-24 11:45:04 +0800

    moodMonitoringApp

---
+++ app/src/main/java/com/example/moodmonitoringapp/MainActivity.kt
@@ -0,0 +1,111 @@
+package com.example.moodmonitoringapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.moodmonitoringapp.databinding.ActivityMainBinding
+import com.example.moodmonitoringapp.fragments.*
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    private val homeFragment = HomeFragment()
+    private val statsFragment = StatsFragment()
+    private val goalsFragment = GoalsFragment()
+    private val profileFragment = ProfileFragment()
+    private val communityFragment = CommunityFragment()
+
+    // FloatingActionButton for all the FABs
+    private lateinit var addFab: FloatingActionButton
+    private lateinit var communityFab: FloatingActionButton
+
+    // These are taken to make visible and invisible along with FABs
+    private lateinit var communityText: TextView
+
+    // to check whether sub FAB buttons are visible or not.
+    private var isAllFabsVisible: Boolean? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(homeFragment)
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener{
+            when(it.itemId){
+                R.id.home -> replaceFragment(homeFragment)
+                R.id.stats ->replaceFragment(statsFragment)
+                R.id.goals -> replaceFragment(goalsFragment)
+                R.id.profile -> replaceFragment(profileFragment)
+            }
+            true
+        }
+
+        // Register all the FABs with their IDs This FAB button is the Parent
+        addFab = findViewById(R.id.add_fab)
+
+        // FAB button
+        communityFab = findViewById(R.id.community_fab)
+
+        // Also register the action name text, of all the FABs.
+        communityText = findViewById(R.id.community_text)
+
+        // Now set all the FABs and all the action name texts as GONE
+        communityFab.visibility = View.GONE
+        communityText.visibility = View.GONE
+
+        // make the boolean variable as false, as all the
+        // action name texts and all the sub FABs are invisible
+        isAllFabsVisible = false
+
+        // We will make all the FABs and action name texts
+        // visible only when Parent FAB button is clicked So
+        // we have to handle the Parent FAB button first, by
+        // using setOnClickListener you can see below
+        addFab.setOnClickListener(View.OnClickListener {
+            (if (!isAllFabsVisible!!) {
+                // when isAllFabsVisible becomes true make all
+                // the action name texts and FABs VISIBLE
+                communityFab.show()
+                communityText.visibility = View.VISIBLE
+
+                // make the boolean variable true as we
+                // have set the sub FABs visibility to GONE
+                true
+            } else {
+                // when isAllFabsVisible becomes true make
+                // all the action name texts and FABs GONE.
+                communityFab.hide()
+                communityText.visibility = View.GONE
+
+                // make the boolean variable false as we
+                // have set the sub FABs visibility to GONE
+                false
+            }).also { isAllFabsVisible = it }
+        })
+
+        // below is the sample action to handle add person FAB. Here it shows simple Toast msg.
+        // The Toast will be shown only when they are visible and only when user clicks on them
+        communityFab.setOnClickListener {
+            replaceFragment(communityFragment)
+        }
+
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        if(fragment!=null ){
+
+            val fragmentTransaction  = supportFragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.fragment_container,fragment)
+            fragmentTransaction.commit()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/moodmonitoringapp/fragments/GoalsFragment.kt
@@ -0,0 +1,23 @@
+package com.example.moodmonitoringapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.moodmonitoringapp.R
+
+class GoalsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        val view =inflater.inflate(R.layout.fragment_goals,container,false)
+
+        return view
+    }
+
+}
